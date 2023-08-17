8693d8fbb23c85906c3350d7268a14f900553226
aydek <kesa0915@hotmail.com>
2023-08-17 08:44:56 +0100

    Update Primary.tsx

--- src-webviews/src/pages/inventory/components/Primary.tsx
+++ src-webviews/src/pages/inventory/components/Primary.tsx
@@ -37,103 +37,58 @@ export const Primary = ({ capacity, items, itemsConfig, drag, secondaryItems, ct
     };
 
     return (
-        <Container className="select-none h-[80%]">
-            <div className="font-marker text-3xl font-semibold m-0 absolute top-0 left-0 -translate-y-10">
-                {translate('inventory', 'player')}
-            </div>
+        <div>
+            <div className="font-marker text-3xl font-semibold m-0 text-whitesmoke">{translate('inventory', 'player')}</div>
 
-            <div
-                className="grid grid-cols-4 gap-1 h-[93%]  overflow-y-scroll overflow-x-hidden px-1 my-1 max-invmd:grid-cols-3"
-                tabIndex={-1}
-            >
-                {items.map((item, i) => (
-                    <div key={i}>
-                        {itemsConfig.length > 0 && item.id !== -1 ? (
-                            <Draggable id={`primary_item:${i}`} onContextMenu={handleRightClick(i)}>
-                                <Droppable
-                                    id={`primary_item:${i}`}
-                                    className="p-1 w-40 h-16 bg-backgroundhover border rounded-sm border-containerBorder flex items-center relative select-none"
-                                >
-                                    {i < 4 ? (
-                                        <div className="absolute px-[3px] text-center rounded top-1 right-1 text-sm text-accent border border-accent">
-                                            {i + 1}
+            <Container className="select-none h-[80%]">
+                <div className="grid grid-cols-4 gap-1 h-[93%]  overflow-y-scroll overflow-x-hidden px-1 my-1 max-invmd:grid-cols-3" tabIndex={-1}>
+                    {items.map((item, i) => (
+                        <div key={i}>
+                            {itemsConfig.length > 0 && item.id !== -1 ? (
+                                <Draggable id={`primary_item:${i}`} onContextMenu={handleRightClick(i)}>
+                                    <Droppable id={`primary_item:${i}`} className="p-1 w-40 h-16 bg-backgroundhover border rounded-sm border-containerBorder flex items-center relative select-none">
+                                        {i < 4 ? <div className="absolute px-[3px] text-center rounded top-1 right-1 text-sm text-accent border border-accent">{i + 1}</div> : null}
+                                        <img className="w-10 h-10 object-contain" src={new URL(`../images/${itemsConfig[item.id].img}.png`, import.meta.url).href} draggable={false} />
+                                        <div className="absolute left-[30%] top-[10px] text-sm">{itemsConfig[item.id].name}</div>
+                                        <div className="absolute left-[30%] top-[25px] text-xs text-gray">{item.description}</div>
+                                        <div className="absolute bottom-[1px] right-[2px] text-[0.8rem]">
+                                            {item.quantity} ( {(itemsConfig[item.id].weight * item.quantity).toFixed(1)} )
                                         </div>
-                                    ) : null}
-                                    <img
-                                        className="w-10 h-10 object-contain"
-                                        src={new URL(`../images/${itemsConfig[item.id].img}.png`, import.meta.url).href}
-                                        draggable={false}
-                                    />
-                                    <div className="absolute left-[30%] top-[10px] text-sm">
-                                        {itemsConfig[item.id].name}
-                                    </div>
-                                    <div className="absolute left-[30%] top-[25px] text-xs text-gray">
-                                        {item.description}
-                                    </div>
-                                    <div className="absolute bottom-[1px] right-[2px] text-[0.8rem]">
-                                        {item.quantity} ( {(itemsConfig[item.id].weight * item.quantity).toFixed(1)} )
-                                    </div>
-                                </Droppable>
-                            </Draggable>
-                        ) : (
-                            <div>
-                                <Droppable
-                                    id={`primary_item:${i}`}
-                                    className="p-1 w-40 h-16 bg-backgroundhover border rounded-sm border-containerBorder flex items-center relative select-none"
-                                >
-                                    {i < 4 ? (
-                                        <div className="absolute px-[3px] text-center rounded top-1 right-1 text-sm text-accent border border-accent">
-                                            {i + 1}
-                                        </div>
-                                    ) : null}
-                                </Droppable>
+                                    </Droppable>
+                                </Draggable>
+                            ) : (
+                                <div>
+                                    <Droppable id={`primary_item:${i}`} className="p-1 w-40 h-16 bg-backgroundhover border rounded-sm border-containerBorder flex items-center relative select-none">
+                                        {i < 4 ? <div className="absolute px-[3px] text-center rounded top-1 right-1 text-sm text-accent border border-accent">{i + 1}</div> : null}
+                                    </Droppable>
+                                </div>
+                            )}
+                        </div>
+                    ))}
+                </div>
+                <DragOverlay
+                    dropAnimation={{
+                        duration: 0,
+                        easing: '',
+                    }}
+                >
+                    {drag.id !== null && drag.type === 'primary_item' && items[drag.id!].id !== -1 && itemsConfig.length > 0 ? (
+                        <div className={'p-1 w-40 h-16 bg-backgroundhover border rounded-sm border-containerBorder flex items-center relative select-none'}>
+                            <img className="w-10 h-10 object-contain" src={new URL(`../images/${itemsConfig[items[drag.id!].id].img}.png`, import.meta.url).href} draggable={false} />
+                            <div className="absolute left-[30%] top-[10px] text-sm">{itemsConfig[items[drag.id!].id].name}</div>
+                            <div className="absolute left-[30%] top-[25px] text-xs text-gray">{items[drag.id!].description}</div>
+                            <div className="absolute bottom-[1px] right-[2px] text-[0.8rem]">
+                                {items[drag.id!].quantity} ( {(itemsConfig[items[drag.id!].id].weight * items[drag.id!].quantity).toFixed(1)} )
                             </div>
-                        )}
-                    </div>
-                ))}
-            </div>
-            <DragOverlay
-                dropAnimation={{
-                    duration: 0,
-                    easing: '',
-                }}
-            >
-                {drag.id !== null &&
-                drag.type === 'primary_item' &&
-                items[drag.id!].id !== -1 &&
-                itemsConfig.length > 0 ? (
-                    <div
-                        className={
-                            'p-1 w-40 h-16 bg-backgroundhover border rounded-sm border-containerBorder flex items-center relative select-none'
-                        }
-                    >
-                        <img
-                            className="w-10 h-10 object-contain"
-                            src={new URL(`../images/${itemsConfig[items[drag.id!].id].img}.png`, import.meta.url).href}
-                            draggable={false}
-                        />
-                        <div className="absolute left-[30%] top-[10px] text-sm">
-                            {itemsConfig[items[drag.id!].id].name}
                         </div>
-                        <div className="absolute left-[30%] top-[25px] text-xs text-gray">
-                            {items[drag.id!].description}
-                        </div>
-                        <div className="absolute bottom-[1px] right-[2px] text-[0.8rem]">
-                            {items[drag.id!].quantity} ({' '}
-                            {(itemsConfig[items[drag.id!].id].weight * items[drag.id!].quantity).toFixed(1)} )
-                        </div>
-                    </div>
-                ) : null}
-            </DragOverlay>
-            <Divider />
-            <div className="flex items-center">
-                <div className="mr-2">{translate('inventory', 'capacity')}:</div>
-                <LinearProgress
-                    color={getItemsWeight() > 80 ? 'bg-error' : getItemsWeight() > 45 ? 'bg-warning' : 'bg-success'}
-                    value={100 - getItemsWeight()}
-                    max={100}
-                />
-            </div>
-        </Container>
+                    ) : null}
+                </DragOverlay>
+                <Divider />
+                <div className="flex items-center">
+                    <div className="mr-2">{translate('inventory', 'capacity')}:</div>
+                    <LinearProgress color={getItemsWeight() > 80 ? 'bg-error' : getItemsWeight() > 45 ? 'bg-warning' : 'bg-success'} value={100 - getItemsWeight()} max={100} />
+                </div>
+            </Container>
+        </div>
     );
 };
