1d2d653d41c4d848eb590b88382aee18edab6bba
aydek <kesa0915@hotmail.com>
2023-08-17 08:44:52 +0100

    Update Equipment.tsx

--- src-webviews/src/pages/inventory/components/Equipment.tsx
+++ src-webviews/src/pages/inventory/components/Equipment.tsx
@@ -4,6 +4,7 @@ import { defaultEquipment } from '../dummyData';
 import { Droppable } from './DragnDrop';
 import { TrashCanIcon } from '../../../components/SVG';
 import { InventoryEvents } from '@events';
+import { Fragment } from 'react';
 
 interface IProps {
     equipment: typeof defaultEquipment;
@@ -18,42 +19,31 @@ export const Equipment = ({ equipment }: IProps) => {
     };
 
     return (
-        <Droppable
-            id="equipment"
-            className="bg-background border border-containerBorder p-1 select-none rounded relative text-whitesmoke w-auto h-[510px] "
-        >
-            <div className="font-marker text-3xl font-semibold m-0 absolute top-0 left-0 -translate-y-10">
-                {translate('inventory', 'clothing')}
-            </div>
-
-            <div className="grid grid-cols-2 gap-1" tabIndex={-1}>
-                {Object.entries(equipment).map(([key, value]) => (
-                    <div
-                        key={key}
-                        className={twMerge(
-                            'w-20 h-20 border border-containerBorder rounded transition duration-200 ease-in-out grid place-items-center  hover:border-accent cursor-pointer',
-                            value[0] === -1 && 'opacity-30 cursor-auto hover:border-containerBorder'
-                        )}
-                        onClick={unequip(key)}
-                        onContextMenu={unequip(key)}
-                    >
-                        <img
-                            className="w-20 h-20"
-                            src={new URL(`../images/${key}.png`, import.meta.url).href}
-                            draggable={false}
-                        />
-                    </div>
-                ))}
-            </div>
-            <div className="w-full rounded bg-background absolute left-0 bottom-[-50px]">
-                <Droppable
-                    id={`destroy_item`}
-                    className="relative w-full border border-containerBorder rounded px-[10px] py-1 flex justify-center items-center"
-                >
-                    <TrashCanIcon className="mr-4" />
-                    <div>{translate('inventory', 'destroy_item')}</div>
-                </Droppable>
-            </div>
-        </Droppable>
+        <div>
+            <div className="font-marker text-3xl font-semibold m-0 text-whitesmoke">{translate('inventory', 'clothing')}</div>
+            <Droppable id="equipment" className="bg-background border border-containerBorder p-1 select-none rounded relative text-whitesmoke w-auto h-[510px] ">
+                <div className="grid grid-cols-2 gap-1" tabIndex={-1}>
+                    {Object.entries(equipment).map(([key, value]) => (
+                        <div
+                            key={key}
+                            className={twMerge(
+                                'w-20 h-20 border border-containerBorder rounded transition duration-200 ease-in-out grid place-items-center  hover:border-accent cursor-pointer',
+                                value[0] === -1 && 'opacity-30 cursor-auto hover:border-containerBorder'
+                            )}
+                            onClick={unequip(key)}
+                            onContextMenu={unequip(key)}
+                        >
+                            <img className="w-20 h-20" src={new URL(`../images/${key}.png`, import.meta.url).href} draggable={false} />
+                        </div>
+                    ))}
+                </div>
+                <div className="w-full rounded bg-background absolute left-0 bottom-[-50px]">
+                    <Droppable id={`destroy_item`} className="relative w-full border border-containerBorder rounded px-[10px] py-1 flex justify-center items-center">
+                        <TrashCanIcon className="mr-4" />
+                        <div>{translate('inventory', 'destroy_item')}</div>
+                    </Droppable>
+                </div>
+            </Droppable>
+        </div>
     );
 };
