66d0f972613034cba072855fafb5abda49d69352
aydek <kesa0915@hotmail.com>
2023-09-01 16:26:29 +0100

    Update functions.ts

--- src/core/server/systems/notifications/functions.ts
+++ src/core/server/systems/notifications/functions.ts
@@ -4,3 +4,7 @@ import { NotificationEvents } from '../../../shared/enums/events/webviewEvents';
 export function showNotification(player: alt.Player, type: string, hide: number, title: string, message: string) {
     alt.emitClient(player, NotificationEvents.show, type, hide, title, message);
 }
+
+export function cancelNotification(player: alt.Player, searchText : string) {
+    alt.emitClient(player, NotificationEvents.cancel, searchText);
+}
