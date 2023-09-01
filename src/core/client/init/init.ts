02165125a9075e47b7f0c775558a18aeae503d8e
aydek <kesa0915@hotmail.com>
2023-09-01 18:02:07 +0100

    Update init.ts

--- src/core/client/init/init.ts
+++ src/core/client/init/init.ts
@@ -2,16 +2,29 @@ import * as alt from 'alt-client';
 import * as native from 'natives';
 import './ipls';
 
-
 native.setPedDefaultComponentVariation(native.playerPedId());
-
 
 alt.setStat('stamina', 100);
 
 alt.everyTick(() => {
     native.disableControlAction(0, 37, true); //weapon wheel [tab]
+    /** Disable all scroll wheel buttons */
+    native.disableControlAction(0, 14, true);
+    native.disableControlAction(0, 15, true);
+    native.disableControlAction(0, 16, true);
+    native.disableControlAction(0, 17, true);
     native.disableControlAction(0, 99, true);
     native.disableControlAction(0, 100, true);
+    native.disableControlAction(0, 115, true);
+    native.disableControlAction(0, 116, true);
+    native.disableControlAction(0, 261, true);
+    native.disableControlAction(0, 262, true);
+    /** Radio controls */
+    native.disableControlAction(0, 81, true);
+    native.disableControlAction(0, 82, true);
+    native.disableControlAction(0, 83, true);
+    native.disableControlAction(0, 84, true);
+
     native.hideHudComponentThisFrame(6); //HUD_VEHICLE_NAME
     native.hideHudComponentThisFrame(7); //HUD_AREA_NAME
     native.hideHudComponentThisFrame(8); //HUD_VEHICLE_CLASS
@@ -19,6 +32,8 @@ alt.everyTick(() => {
     alt.beginScaleformMovieMethodMinimap('SETUP_HEALTH_ARMOUR');
     native.scaleformMovieMethodAddParamInt(3);
     native.endScaleformMovieMethod();
+    if (native.isDisabledControlJustPressed(0, 261)) console.log('up');
+    if (native.isDisabledControlJustPressed(0, 262)) console.log('down');
 });
 
 alt.HandlingData.getForHandlingName(alt.hash('adder')).initialDriveForce = 0.45;
